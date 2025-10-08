c6bc2411dfdc19e036b533d3351b491da9511109
JiRaYa_OG <115741454+JiRaYaOG@users.noreply.github.com>
2025-10-08 17:00:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,22 @@ for (let i = 0; i < PARTICLE_COUNT; i++) {
 
 function animate() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-  ctx.fillRect(0
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(0,255,255,0.8)";
+  particles.forEach(p => {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+    ctx.fill();
+
+    p.x += p.dx;
+    p.y += p.dy;
+
+    if (p.x < 0 || p.x > canvas.width) p.dx *= -1;
+    if (p.y < 0 || p.y > canvas.height) p.dy *= -1;
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
